6c4f4e54f8a721a94dac83d898eaa827bd1884fa
Veit Heller <veit@veitheller.de>
2025-02-04 09:38:23 +0100

    add scripts and definitions for gemstone

---
+++ scripts/installGt4Llm_rowan3.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+### Install gt4llm from Tonel files into a Rowan-enabled stone
+### Exits with 0 if success, 1 if failed
+
+gt4GemstoneHome=${ROWAN_PROJECTS_HOME}/gt4llm
+## Topaz refuses to exit from script if input is stdin, so redirect from /dev/zero
+topaz -l -I ${gt4GemstoneHome}/scripts/loginSystemUser.topaz  -S ${gt4GemstoneHome}/scripts/installGT4Llm.topaz < /dev/null
+if [ $? = 0 ]
+    then
+        echo gt4llm loaded
+    else
+        echo !!!!!!!!!!!!!!
+        echo INSTALL FAILED for gt4llm
+        echo !!!!!!!!!!!!!!
+        exit 1
+    fi
+
